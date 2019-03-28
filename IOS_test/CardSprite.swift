a4fe43413b1db92ae5624ccaa69ee9fde3f9beee
joan <joanlojo.7@gmail.com>
2019-03-28 15:30:56 +0100

    Inicio del botón

--- IOS_test/CardSprite.swift
+++ IOS_test/CardSprite.swift
@@ -27,4 +27,15 @@ class CardSprite: SKSpriteNode {
         fatalError()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //let action = SKAction.scale(by: 0.9, duration: 0.1)
+        //run(action)
+        print("press")
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //let action = SKAction.scale(by: 1.0/0.9, duration: 0.1)
+        //run(action)
+        print("notpress")
+    }
 }
